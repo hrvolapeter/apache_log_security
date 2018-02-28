20b33e265bb4215962d0fe155ba0a65ce5f7b799
Travis <travis@Traviss-Mac-463.local>
2018-02-28 19:27:29 +0000

    Automatic Travis documentation build

--- master/implementors/core/ops/arith/trait.Mul.js
+++ master/implementors/core/ops/arith/trait.Mul.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
-implementors["crypto"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a> for <a class=\"struct\" href=\"crypto/curve25519/struct.Fe.html\" title=\"struct crypto::curve25519::Fe\">Fe</a>",];
-implementors["time"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i32.html\">i32</a>&gt; for <a class=\"struct\" href=\"time/struct.Duration.html\" title=\"struct time::Duration\">Duration</a>",];
+implementors["crypto"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a> for <a class=\"struct\" href=\"crypto/curve25519/struct.Fe.html\" title=\"struct crypto::curve25519::Fe\">Fe</a>",synthetic:false,types:["crypto::curve25519::Fe"]},];
+implementors["time"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i32.html\">i32</a>&gt; for <a class=\"struct\" href=\"time/struct.Duration.html\" title=\"struct time::Duration\">Duration</a>",synthetic:false,types:["time::duration::Duration"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
