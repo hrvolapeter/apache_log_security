ed706c23b31655b3f55d11102b668e280bd0f1dd
Travis CI User <travis@example.org>
2018-04-13 19:16:26 +0000

    Automatic Travis documentation build

--- master/security_framework_sys/certificate/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["SecCertificateCopyCommonName",""],["SecCertificateCopyData",""],["SecCertificateCopyPublicKey",""],["SecCertificateCopySubjectSummary",""],["SecCertificateCreateWithData",""],["SecCertificateGetTypeID",""]]});
