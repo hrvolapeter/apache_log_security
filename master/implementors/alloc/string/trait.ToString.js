29ed1763f837909d1c919743da129d52640c8bac
Travis <travis@Traviss-Mac-463.local>
2018-04-29 11:43:43 +0000

    Automatic Travis documentation build

--- master/implementors/alloc/string/trait.ToString.js
+++ master/implementors/alloc/string/trait.ToString.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["elastic_types"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"elastic_types/geo/mapping/struct.Distance.html\" title=\"struct elastic_types::geo::mapping::Distance\">Distance</a>",synthetic:false,types:["elastic_types::geo::mapping::Distance"]},];
+implementors["elastic_types"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"elastic_types/geo/mapping/struct.Distance.html\" title=\"struct elastic_types::geo::mapping::Distance\">Distance</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
