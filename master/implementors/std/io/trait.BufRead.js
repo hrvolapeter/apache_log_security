a978300fe9e474653d3b41bc5599d8423937912b
Travis CI User <travis@example.org>
2018-04-29 11:17:24 +0000

    Automatic Travis documentation build

--- master/implementors/std/io/trait.BufRead.js
+++ master/implementors/std/io/trait.BufRead.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bufstream"] = [{text:"impl&lt;S:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a> for <a class=\"struct\" href=\"bufstream/struct.BufStream.html\" title=\"struct bufstream::BufStream\">BufStream</a>&lt;S&gt;",synthetic:false,types:["bufstream::BufStream"]},];
+implementors["bufstream"] = ["impl&lt;S:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a> for <a class=\"struct\" href=\"bufstream/struct.BufStream.html\" title=\"struct bufstream::BufStream\">BufStream</a>&lt;S&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
