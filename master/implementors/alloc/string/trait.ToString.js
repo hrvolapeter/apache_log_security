819d23157bc9db50664d91e5bd876729b731a803
Travis <travis@Traviss-Mac-463.local>
2018-05-06 12:08:00 +0000

    Automatic Travis documentation build

--- master/implementors/alloc/string/trait.ToString.js
+++ master/implementors/alloc/string/trait.ToString.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
-implementors["elastic_types"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"elastic_types/geo/mapping/struct.Distance.html\" title=\"struct elastic_types::geo::mapping::Distance\">Distance</a>",synthetic:false,types:["elastic_types::geo::mapping::Distance"]},];
-implementors["openssl"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"openssl/x509/extension/enum.Extension.html\" title=\"enum openssl::x509::extension::Extension\">Extension</a>",synthetic:false,types:["openssl::x509::extension::Extension"]},];
+implementors["elastic_types"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"elastic_types/geo/mapping/struct.Distance.html\" title=\"struct elastic_types::geo::mapping::Distance\">Distance</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
